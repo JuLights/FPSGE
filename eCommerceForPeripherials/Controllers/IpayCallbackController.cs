90c87b694c70bd007efc0b51fc2556398a089425
JuLights <davidjugeli23@gmail.com>
2022-03-26 01:45:12 +0400

    last update

---
+++ eCommerceForPeripherials/Controllers/IpayCallbackController.cs
@@ -0,0 +1,34 @@
+﻿using BankOfGeorgia.IpayClient;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceForPeripherials.Controllers
+{
+    public class IpayCallbackController : Controller
+    {
+        private readonly IBankOfGeorgiaIpayClient _iPayClient;
+
+        public IpayCallbackController(IBankOfGeorgiaIpayClient iPayClient)
+        {
+            _iPayClient = iPayClient;
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public async Task<IActionResult> Payment([FromForm] PaymentCallbackResult result)
+        {
+            var kk = result;
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public async Task<IActionResult> Refund([FromForm] RefundCallbackResult result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- eCommerceForPeripherials/Controllers/ShopController.cs
+++ eCommerceForPeripherials/Controllers/ShopController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace eCommerceForPeripherials.Controllers
@@ -39,30 +40,30 @@ namespace eCommerceForPeripherials.Controllers
             var checkoutItem = _db.Items.Where(x => x.Id == Id).FirstOrDefault();
             string prdctId = checkoutItem.Id.ToString();
 
-            var orderItem = new List<OrderItem>();
-            orderItem.Add(new OrderItem
+            var order = new OrderRequest()
             {
-                Price = Convert.ToDecimal(checkoutItem.Price),
-                Description = checkoutItem.Description,
-                ProductId = checkoutItem.Id.ToString(),
-                Quantity = 1
-            });
+                Intent = Intent.Authorize,
+                Currency = IPayCurrency.GEL,
+                Items = new[]
+                {
+                    new OrderItem() { Price= Convert.ToDecimal(checkoutItem.Price), Description = checkoutItem.Description, Quantity = 1, ProductId = checkoutItem.Id.ToString() }
+                },
+                Locale = "ka",
+                ShopOrderId = Guid.NewGuid().ToString("N"),
+                RedirectUrl = "https://localhost:44371//IpayCallback/Payment",
+                ShowShopOrderIdOnExtract = true,
+                CaptureMethod = CaptureMethod.Automatic,
+            };
+
+            //await _iPayClient.AuthenticateAsync();
 
             //await _iPayClient.AuthenticateAsync(); //es aucilebelia
 
-            var shporderid = checkoutItem.Name + "//" + checkoutItem.Brand + "//" + checkoutItem.Id.ToString();
-            var response = await _iPayClient.MakeOrderAsync(new OrderRequest
-            {
-                Intent = Intent.Capture,
-                CaptureMethod = CaptureMethod.Automatic,
-                Currency = IPayCurrency.GEL,
-                Items = orderItem,
-                Locale = Locale.KA,
-                ShopOrderId = checkoutItem.Name + "//" + checkoutItem.Brand + "//" + checkoutItem.Id.ToString(),
-                 ShowShopOrderIdOnExtract = true,
-                 RedirectUrl = $"https://demo.ipay.ge?shop_order_id={shporderid}",
-            });
-            var redirectUrl = $"https://demo.ipay.ge?shop_order_id={shporderid}"; // responsedan unda mivigo GetRedirectUrl(); metodit
+            //var shporderid = checkoutItem.Name + "//" + checkoutItem.Brand + "//" + checkoutItem.Id.ToString();
+            var response = await _iPayClient.MakeOrderAsync(order);
+
+            // When succeeded redirect to bank page
+            var redirectUrl = response.GetRedirectUrl();
 
             return Redirect(redirectUrl);
         }
